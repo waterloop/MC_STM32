0cee8b33f4135a70fe3836410602c8f1f94d9870
chawthri <59934531+chawthri@users.noreply.github.com>
2020-11-17 23:59:29 -0500

    added l432 code

---
+++ STM32L476RG/Core/Inc/DEFINES.h
@@ -0,0 +1,13 @@
+/*
+ * DEFINES.h
+ *
+ *  Created on: Aug. 13, 2020
+ *      Author: Chawthri
+ */
+
+#ifndef INC_DEFINES_H_
+#define INC_DEFINES_H_
+
+#define MODE 0 /* 0: Fixed Output 1: V/F Open Loop 2: V/F Closed LoopW*/
+
+#endif /* INC_DEFINES_H_ */
