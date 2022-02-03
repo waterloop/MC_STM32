d6b178ca2b51f40f0295a57fea5f3a4413633d63
kani-tech <kanishksk@gmail.com>
2022-02-03 18:31:55 -0500

    add different states as typedef enum

--- STM32F405_Logic_Board/state_machine.h
+++ STM32F405_Logic_Board/state_machine.h
@@ -1,17 +1,48 @@
-using namespace std;
+/*
+ * state_machine.h
+ *
+ *    Created on: Jul. 11, 2021
+ *            Author: Kanishk Kashyap
+ */
 
 #ifndef _STATE_MACHINE_H_
 #define _STATE_MACHINE_H_
 
+using namespace std;
+
+typedef enum
+{
+    Initialize,
+    InitializeFault,
+    Idle,
+    Brake,
+    AutoPilot,
+    ManualControl,
+    EmergencyBrake,
+    Run,
+    Stop,
+    Sleep,
+    NormalDangerFault,
+    SevereDangerFault,
+    NoFault,
+    Reset
+} State_t;
+
+typedef State_t (*pfEvent)(void);
 class MCStateMachine
 {
+public:
+    State_t State;
+    pfEvent Event;
 
-    void InitializeEvent(void);
-    void InitializeFaultEvent(void);
-    void IdleEvent(void);
-    void MinorDangerFaultEvent(void);
-    void AutoPilotEvent(void);
-    void SevereDangerFaultEvent(void);
-    void ManualControlEvent(void);
-    void AutoPilotEvent(void);
+    State_t InitializeEvent(void);
+    State_t InitializeFaultEvent(void);
+    State_t IdleEvent(void);
+    State_t MinorDangerFaultEvent(void);
+    State_t AutoPilotEvent(void);
+    State_t SevereDangerFaultEvent(void);
+    State_t ManualControlEvent(void);
+    State_t AutoPilotEvent(void);
 };
+
+#endif
