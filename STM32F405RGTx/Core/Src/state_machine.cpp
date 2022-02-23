38d7bd6cc1a3c49603d4c1cb48ad3d18dd31a239
kani-tech <kanishksk@gmail.com>
2022-02-23 12:23:58 -0500

    close to finish state_machine

--- STM32F405RGTx/Core/Src/state_machine.cpp
+++ STM32F405RGTx/Core/Src/state_machine.cpp
@@ -15,27 +15,15 @@ void MCStateMachine::SetLedColour(float R, float G, float B)
 
 void SendCANHeartbeat(void)
 {
-    float avg_cell_temp = 0;
-    for (uint8_t i = 0; i < NUM_CELLS; i++)
-    {
-        avg_cell_temp += motor_controller_data.battery.cells[i].temp;
-    }
-    avg_cell_temp /= NUM_CELLS;
+    float avg_MC_current = (MotorControllerData.pIa + MotorControllerData.pIb + MotorControllerData.pIc) / 3;
 
-    CANFrame tx_frame0 = CANFrame_init(BATTERY_PACK_CURRENT.id);
-    CANFrame_set_field(&tx_frame0, BATTERY_PACK_CURRENT, FLOAT_TO_UINT(global_bms_data.battery.current));
-    CANFrame_set_field(&tx_frame0, CELL_TEMPERATURE, FLOAT_TO_UINT(avg_cell_temp));
+    CANFrame tx_frame0 = CANFrame_init(POD_SPEED.id);
+    CANFrame_set_field(&tx_frame0, POD_SPEED, FLOAT_TO_UINT(MotorControllerData.cur_speed));
+    CANFrame_set_field(&tx_frame0, MOTOR_CURRENT, FLOAT_TO_UINT(MotorControllerData.cur_speed));
 
-    CANFrame tx_frame1 = CANFrame_init(BATTERY_PACK_VOLTAGE.id);
+    CANFrame tx_frame1 = CANFrame_init(BATTERY_CURRENT.id);
+    CANFrame_set_field(&tx_frame1, BATTERY_CURRENT, FLOAT_TO_UINT(global_bms_data.battery.current));
     CANFrame_set_field(&tx_frame1, BATTERY_PACK_VOLTAGE, FLOAT_TO_UINT(global_bms_data.battery.voltage));
-    CANFrame_set_field(&tx_frame1, STATE_OF_CHARGE, FLOAT_TO_UINT(global_bms_data.battery.soc));
-
-    CANFrame tx_frame2 = CANFrame_init(BUCK_TEMPERATURE.id);
-    CANFrame_set_field(&tx_frame2, BUCK_TEMPERATURE, FLOAT_TO_UINT(global_bms_data.buck_temp));
-    // CANFrame_set_field(&tx_frame2, BMS_CURRENT, FLOAT_TO_UINT(0));
-
-    CANFrame tx_frame3 = CANFrame_init(MC_CAP_VOLTAGE.id);
-    CANFrame_set_field(&tx_frame3, MC_CAP_VOLTAGE, FLOAT_TO_UINT(global_bms_data.mc_cap_voltage));
 
     if (CANBus_put_frame(&tx_frame0) != HAL_OK)
     {
@@ -45,20 +33,11 @@ void SendCANHeartbeat(void)
     {
         Error_Handler();
     }
-    if (CANBus_put_frame(&tx_frame2) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    if (CANBus_put_frame(&tx_frame3) != HAL_OK)
-    {
-        Error_Handler();
-    }
 }
 
 // Returns normal fault state or no fault based on current, voltage, and temperature measurements
 State_t MCStateMachine::NormalFaultChecking(void)
 {
-    float current = global_bms_data.battery.current;
     int overvolt_faults = 0;
     int undervolt_faults = 0;
     int temp_faults = 0;
@@ -126,7 +105,6 @@ State_t MCStateMachine::NormalFaultChecking(void)
 
 State_t MCStateMachine::SevereFaultChecking(void)
 {
-    float current = global_bms_data.battery.current;
     int overvolt_faults = 0;
     int undervolt_faults = 0;
     int temp_faults = 0;
@@ -218,10 +196,11 @@ State_t MCStateMachine::IdleEvent(void)
         CANFrame rx_frame = CANBus_get_frame();
         uint8_t state_id = CANFrame_get_field(&rx_frame, STATE_ID);
         idle_state_id = state_id;
-        if (state_id == AUTO_PILOT)
+        if (state_id == AUTO_PILOT) // how do i get length of track
         {
-            return Run;
+            return AutoPilot;
         }
+        else if (state_id == MANUAL_OPERATION_WAITING)
     }
 
     return Idle;
@@ -233,7 +212,7 @@ State_t AutoPilotEvent(void)
     SetLEDColour(50.0, 50.0, 0.0);
 
     // Send ACK on CAN when stop complete
-    CANFrame tx_frame = CANFrame_init(BMS_STATE_CHANGE_ACK_NACK);
+    CANFrame tx_frame = CANFrame_init(MOTOR_CONTROLLER_STATE_CHANGE_ACK_NACK);
     CANFrame_set_field(&tx_frame, STATE_CHANGE_ACK_ID, run_state_id);
     CANFrame_set_field(&tx_frame, STATE_CHANGE_ACK, 0x00);
     if (CANBus_put_frame(&tx_frame) != HAL_OK)
@@ -246,14 +225,149 @@ State_t AutoPilotEvent(void)
     {
         CANFrame rx_frame = CANBus_get_frame();
         uint8_t state_id = CANFrame_get_field(&rx_frame, STATE_ID);
-        if (state_id == EMERGENCY_BRAKE)
+        if (state_id == EMERGENCY_BRAKE || state_id == SYSTEM_FAILURE)
+        {
+            State_t normal_fault_check = NormalFaultChecking();
+            State_t severe_fault_check = SevereFaultChecking();
+            if (severe_fault_check == SevereDangerFault)
+            {
+                return severe_fault_check;
+            }
+            else if (normal_fault_check == NormalDangerFault)
+            {
+                return normal_fault_check;
+            }
+        }
+        else if (state_id == BRAKING)
+        {
+            return Idle;
+        }
+    }
+    return Idle;
+}
+
+State_t ManualControl(void)
+{
+    // Set LED colour to blue
+    SetLEDColour(0, 0, 50.0);
+
+    // Send ACK on CAN when stop complete
+    CANFrame tx_frame = CANFrame_init(MOTOR_CONTROLLER_STATE_CHANGE_ACK_NACK);
+    CANFrame_set_field(&tx_frame, STATE_CHANGE_ACK_ID, run_state_id);
+    CANFrame_set_field(&tx_frame, STATE_CHANGE_ACK, 0x00);
+    if (CANBus_put_frame(&tx_frame) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    // Receive CAN frame
+    if (!Queue_empty(&RX_QUEUE))
+    {
+        CANFrame rx_frame = CANBus_get_frame();
+        uint8_t state_id = CANFrame_get_field(&rx_frame, STATE_ID);
+        if (state_id == EMERGENCY_BRAKE || state_id == SYSTEM_FAILURE)
+        {
+            State_t normal_fault_check = NormalFaultChecking();
+            State_t severe_fault_check = SevereFaultChecking();
+            if (severe_fault_check == SevereDangerFault)
+            {
+                return severe_fault_check;
+            }
+            else if (normal_fault_check == NormalDangerFault)
+            {
+                return normal_fault_check;
+            }
+        }
+        else if (state_id == BRAKING)
+        {
+            return Idle;
+        }
+    }
+    return Idle;
+}
+
+State_t InitializeFaultEvent(void)
+{
+
+    CANFrame tx_frame = CANFrame_init(MC_SEVERITY_CODE.id);
+    CANFrame_set_field(&tx_frame, MC_SEVERITY_CODE, INITIAL_FAULT); // dummy
+    CANFrame_set_field(&tx_frame, ERROR_CODE, mc_error_code);
+    SetLEDColour(50.0, 0.0, 0.0);
+    // Receive CAN frame
+    if (!Queue_empty(&RX_QUEUE))
+    {
+        CANFrame rx_frame = CANBus_get_frame();
+        uint8_t state_id = CANFrame_get_field(&rx_frame, STATE_ID);
+        if (state_id == RESTING)
         {
             return Idle;
         }
         else
         {
-            return Stop;
+            return InitializeFault;
         }
     }
-    return Stop;
+    return InitializeFault;
+}
+
+State_t NormalDangerFaultEvent(void)
+{
+    // Set LED colour to red
+    SetLEDColour(50.00, 0.0, 0.0);
+
+    // Report fault on CAN
+    CANFrame tx_frame = CANFrame_init(BMS_SEVERITY_CODE.id);
+    CANFrame_set_field(&tx_frame, MC_SEVERITY_CODE, DANGER);
+    CANFrame_set_field(&tx_frame, ERROR_CODE, mc_error_code);
+    if (CANBus_put_frame(&tx_frame) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    // Receive CAN frame
+    if (!Queue_empty(&RX_QUEUE))
+    {
+        CANFrame rx_frame = CANBus_get_frame();
+        uint8_t state_id = CANFrame_get_field(&rx_frame, STATE_ID);
+        if (state_id == RESTING)
+        {
+            return Idle;
+        }
+        else
+        {
+            return NormalDangerFault;
+        }
+    }
+    return NormalDangerFault;
+}
+
+State_t SevereDangerFault(void)
+{
+    // Set LED colour to red
+    SetLEDColour(50.00, 0.0, 0.0);
+
+    // Report fault on CAN
+    CANFrame tx_frame = CANFrame_init(MC_SEVERITY_CODE.id);
+    CANFrame_set_field(&tx_frame, SEVERITY_CODE, DANGER);
+    CANFrame_set_field(&tx_frame, ERROR_CODE, mc_error_code);
+    if (CANBus_put_frame(&tx_frame) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    // Receive CAN frame
+    if (!Queue_empty(&RX_QUEUE))
+    {
+        CANFrame rx_frame = CANBus_get_frame();
+        uint8_t state_id = CANFrame_get_field(&rx_frame, STATE_ID);
+        if (state_id == RESTING)
+        {
+            return Idle;
+        }
+        else
+        {
+            return NormalDangerFault;
+        }
+    }
+    return NormalDangerFault;
 }
