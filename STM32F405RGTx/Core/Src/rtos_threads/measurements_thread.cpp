34823d565e005983ba6ba1e4018fa30857d2f36c
Dev Patel <Dev597704@gmail.com>
2022-02-26 16:14:06 -0500

    update ioc file and measurements thread with voltage and current

--- STM32F405RGTx/Core/Src/rtos_threads/measurements_thread.cpp
+++ STM32F405RGTx/Core/Src/rtos_threads/measurements_thread.cpp
@@ -7,6 +7,10 @@
 #include "threads.hpp"
 
 // TODO: add defines here for conversions..
+#define CURRENT_SENSE_RESISTANCE                1E-3
+#define ADC_TO_VOLTAGE(adc)                     ( adc * (3.3/(1 << 12)) )
+#define VOLTAGE_TO_CURRENT(voltage)             (voltage / CURRENT_SENSE_RESISTANCE)
+#define VOLTAGE_DIVIDER_CONVERSION(voltage)     ( voltage * 17.25 )
 
 RTOSThread MeasurementsThread::thread;
 uint16_t MeasurementsThread::ADC_buffer[];
@@ -40,14 +44,46 @@ void MeasurementsThread::processData() {
         uint32_t val = (sum/ADC_DECIMATION_COEFF);
 
         switch (i) {
-            // TODO need to figure out which variables need to be loaded for each 'i'
-            // global_mc_data.pVa = ...;
-            // global_mc_data.pVb = ...;
-            // global_mc_data.pVc = ...;
+
+            // read and convert phase voltages and dc voltage
             case 0:
+               g_mc_data.pVa =          VOLTAGE_DIVIDER_CONVERSION( ADC_TO_VOLTAGE( val ) );
+            case 1:
+               g_mc_data.pVb =          VOLTAGE_DIVIDER_CONVERSION( ADC_TO_VOLTAGE( val ) );
+            case 2:
+               g_mc_data.pVc =          VOLTAGE_DIVIDER_CONVERSION( ADC_TO_VOLTAGE( val ) );
+            case 3:
+               g_mc_data.dc_voltage =   VOLTAGE_DIVIDER_CONVERSION( ADC_TO_VOLTAGE( val ) );
+
+            // read and convert phase currents
+            case 4:
+               g_mc_data.pIa =          VOLTAGE_TO_CURRENT( val );
+            case 5:
+               g_mc_data.pIb =          VOLTAGE_TO_CURRENT( val );
+            case 6:
+               g_mc_data.pIc =          VOLTAGE_TO_CURRENT( val );
+
+            // read and convert adc temperatures
+            // TODO: need to figure out how to convert these values
+            // NOTE: the ordering might change depending physical arrangement later on
+            case 7:
+               g_mc_data.dc_cap_temp =  111;
+            case 8:
+               g_mc_data.fet_temps[0] = 111;
+            case 9:
+               g_mc_data.fet_temps[1] = 111;
+            case 10:
+               g_mc_data.fet_temps[2] = 111;
+
+            case 111:
                 printf("Val = %lx\r\n", val);
 
         }
+
+        // update with functions once acceleration code is written
+        // g_mc_data.curr_accel =
+        // g_mc_data.curr_speed =
+        // g_mc_data.curr_pos =
     }
 }
 
